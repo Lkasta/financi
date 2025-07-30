0ea23035a2e00017635c57283733b877be15450b
Lkasta <lkastabackup@gmail.com>
2025-07-29 21:57:00 -0300

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter, Fira_Code } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const firaCode = Fira_Code({
+  variable: "--font-fira-code",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "financi",
+  description:
+    "Dashboard financeiro com login, filtros dinâmicos e visualização de transações.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${inter.variable} ${firaCode.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="">
+      <p className="text-primary font-bold">
+        financ<span className="text-violet-500">i</span>
+      </p>
+    </div>
+  );
+}
