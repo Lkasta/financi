49f66bf1a46291d23ecd9fa4e0fdeece1cdda3c3
Lkasta <lkastabackup@gmail.com>
2025-07-31 21:33:17 -0300

    feat: added transactions route

---
+++ src/app/transactions/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
+import { AgGridReact } from "ag-grid-react";
+import { themeQuartz, ColDef } from "ag-grid-community";
+
+import transactions from "@/data/transactions.json";
+
+type Transaction = {
+  date: string;
+  amount: string;
+  transaction_type: string;
+  currency: string;
+  account: string;
+  industry: string;
+  state: string;
+};
+
+const initialValues: Transaction = {
+  date: "",
+  amount: "",
+  transaction_type: "",
+  currency: "",
+  account: "",
+  industry: "",
+  state: "",
+};
+
+export default function Transactions() {
+  const [dados, setDados] = useState<Transaction[]>([initialValues]);
+
+  const [colDefs] = useState<ColDef<Transaction>[]>([
+    {
+      field: "date",
+      headerName: "Data",
+      flex: 1,
+      valueFormatter: (e) => e.value && format(e.value, "dd/MM/yyyy"),
+    },
+    { field: "amount", headerName: "Valor", flex: 1 },
+    { field: "transaction_type", headerName: "Tipo de Transação", flex: 1 },
+    { field: "currency", headerName: "Moeda", flex: 1 },
+    { field: "account", headerName: "Conta", flex: 1 },
+    { field: "industry", headerName: "Empresa", flex: 1 },
+    { field: "state", headerName: "Estado", flex: 1 },
+  ]);
+
+  useEffect(() => {
+    setDados(transactions as Transaction[]);
+  }, []);
+
+  return (
+    <div className="container mx-auto w-ful min-h-screen pt-14 px-4">
+      <div className="w-full h-[750px] rounded-xl shadow-md">
+        <AgGridReact
+          rowHeight={44}
+          headerHeight={48}
+          rowData={dados}
+          theme={themeQuartz}
+          columnDefs={colDefs}
+          className="rounded-t-xl"
+        />
+      </div>
+    </div>
+  );
+}
